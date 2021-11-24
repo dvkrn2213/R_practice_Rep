c98170fc2b729d56cf3777f58b58b41f5365b685
dvkrn2213 <dvkrn2213@gmail.com>
2021-11-24 15:00:10 +0530

    2Nd

--- TopMovies_webscarpping.R
+++ TopMovies_webscarpping.R
@@ -2,13 +2,14 @@ library(rvest)
 
 link<-read_html("https://editorial.rottentomatoes.com/guide/best-netflix-movies-to-watch-right-now/")
 
-title<-link %>%html_nodes(".article_movie_title a")%>%html_text()
+Title<-link %>%html_nodes(".article_movie_title a")%>%html_text()
 
 
-CC<-link %>%html_nodes(".critics-consensus")%>%html_text()
+Critic_Concensus <-link %>%html_nodes(".critics-consensus")%>%html_text()
 
 synopsis<-link %>%html_nodes(".synopsis")%>%html_text()
 
-director<-link %>%html_nodes(".director .descriptor+ a")%>%html_text()
+Director<-link %>%html_nodes(".director .descriptor+ a")%>%html_text()
 
-df<-data.frame(title,CC,synopsis,director)
+df<-data.frame(Title,Critic_Concensus,synopsis,director)
+
